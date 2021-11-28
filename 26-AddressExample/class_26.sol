0ff56d40d7bb75cd21357acbaf529298aa404166
Natan Votre <nvt@brickabode.com>
2021-11-28 07:47:51 -0300

    Class 26: Address Example

---
+++ 26-AddressExample/class_26.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.11;
+
+contract MyContract {
+    uint256 public myUint;
+    bool public myBool;
+    address public myAddress;
+
+    function setMyUint(uint256 _myUint) public {
+        myUint = _myUint;
+    }
+
+    function incrementMyUint() public {
+        myUint += 1;
+    }
+
+    function decrementMyUint() public {
+        myUint -= 1;
+    }
+
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+
+    function setAddress(address _address) public {
+        myAddress = _address;
+    }
+
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
+}
